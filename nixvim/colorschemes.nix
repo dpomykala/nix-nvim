cb12b314041f28d366ec6a69a717cb7b03cb3410
Damian Pomykała <168227269+dpomykala@users.noreply.github.com>
2025-11-11 23:56:40 +0100

    Remove the catppuccin integration config for blink.cmp
    The blink.cmp integration is enabled by default now.

--- nixvim/colorschemes.nix
+++ nixvim/colorschemes.nix
@@ -10,9 +10,6 @@
       flavour = "macchiato";
 
       integrations = lib.mkMerge [
-        # TODO: Remove when the default value is changed to true
-        (lib.mkIf config.plugins.blink-cmp.enable {blink_cmp = true;})
-
         (lib.mkIf config.plugins.grug-far.enable {grug_far = true;})
 
         # TODO: Remove when the default value is changed as below
